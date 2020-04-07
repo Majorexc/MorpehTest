b10aa8ff415d2682bdb20331dfb1372cb569e445
Eugene <me@airion.games>
2020-04-07 18:48:43 +0300

    Added buttons click windows switching

--- Assets/_morpehTest/Scripts/Systems/ScrollWindowsSystem.cs
+++ Assets/_morpehTest/Scripts/Systems/ScrollWindowsSystem.cs
@@ -16,38 +16,39 @@ public sealed class ScrollWindowsSystem : UpdateSystem {
 
     public override void OnAwake() {
         _scrollRectFilter = World.Filter.With<MainScrollComponent>();
-        _windowsFilter = World.Filter.With<WindowComponent>();
+        _windowsFilter = World.Filter.With<MainWindowComponent>();
     }
 
     public override void OnUpdate(float deltaTime) {
-        
-        // if (_vector2Global) {
-            // Debug.Log(_vector2Global.BatchedChanges.Peek());
-        // }
         foreach (var entity in _scrollRectFilter) {
             ref var scrollRectComponent = ref entity.GetComponent<MainScrollComponent>();
             if (scrollRectComponent.EndDragGlobal) {
                 var rectPos = scrollRectComponent.ScrollRect.horizontalNormalizedPosition;
                 var nearestPos = 1f;
                 var nearestId = 0f;
-                for (int i = 0; i < _windowsFilter.Length; i++) {
-                    var position = i / ((float)_windowsFilter.Length - 1);
+                foreach (var windowEntity in _windowsFilter) {
+                    var window = windowEntity.GetComponent<MainWindowComponent>();
+                    var position = window.Index / ((float)_windowsFilter.Length - 1);
                     if (Mathf.Abs(rectPos - position) <= nearestPos) {
-                        nearestId = i;
+                        nearestId = window.Index;
                         nearestPos = Mathf.Abs(rectPos - position);
                     }
                 }
 
                 var pos = nearestId / ((float)_windowsFilter.Length - 1);
                 scrollRectComponent.ScrollRect.horizontalNormalizedPosition = pos;
-                // var orderedWindows = _windowsFilter.OrderBy((e)=>e.GetComponent<WindowComponent>().canvasGroup.tr)
-                // var windows = _windowsFilter.Length;
-                // var positionOnScroll = scrollRectComponent.ScrollRect.sc
-                // scrollRectComponent.ScrollRect.
             }
-            
-            // scrollRectComponent.ScrollRect.
         }
-        
+
+        foreach (var windowEntity in _windowsFilter) {
+            ref var window = ref windowEntity.GetComponent<MainWindowComponent>();
+            if (window.OpenEvent) {
+                foreach (var rectEntity in _scrollRectFilter) {
+                    ref var scrollRectComponent = ref rectEntity.GetComponent<MainScrollComponent>();
+                    var pos = window.Index / ((float) _windowsFilter.Length - 1);
+                    scrollRectComponent.ScrollRect.horizontalNormalizedPosition = pos;
+                }
+            }
+        }
     }
 }
