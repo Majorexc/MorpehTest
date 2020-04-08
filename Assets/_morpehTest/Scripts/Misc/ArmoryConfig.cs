115bab42db5c4acd203ccd03eefebf80019953bd
Eugene <me@airion.games>
2020-04-08 13:07:09 +0300

    Added armory objects

---
+++ Assets/_morpehTest/Scripts/Misc/ArmoryConfig.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(order = 0, fileName = "New armory config", menuName = "MorpehTest/New Armory config")]
+public class ArmoryConfig : ScriptableObject {
+    public ArmoryItem[] Items => _items;
+    
+    [SerializeField] ArmoryItem[] _items = default;
+}
---
+++ Assets/_morpehTest/Scripts/Misc/ArmoryItem.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(order = 0, fileName = "New armory item", menuName = "MorpehTest/New Armory")]
+public class ArmoryItem : ScriptableObject {
+    public string Name => _name;
+    
+    [SerializeField] string _name = "No name";
+}
